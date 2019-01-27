4d00e5de80113e45687d89db354d70022dc0f207
Don <don.f.stegall@gmail.com>
2019-01-26 22:14:17 -0500

    edit #0001 added Material modules and basic hello-goodbye functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'hello-goodbye-angular-material';
+
+  private inputText: string = null;
+
+  public handleHello() {
+    this.inputText = 'Hello';
+  }
+
+  public handleGoodbye() {
+    this.inputText = 'Goodbye';
+  }
+
+  public handleUpperCase() {
+    this.inputText = this.inputText.toUpperCase();
+  }
+
+  public handleLowerCase() {
+    this.inputText = this.inputText.toLowerCase();
+  }
+
+  public handleMessage() {
+    alert(this.inputText);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
